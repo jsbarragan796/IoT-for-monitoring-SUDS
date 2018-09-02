ecef7bdebe1cc9c1cc6b9fadb214a351871ebbde
Juan Guzmán <jdguzmans@hotmail.com>
2018-09-02 17:09:37 -0500

    hj

--- routes/measurement.js
+++ routes/measurement.js
@@ -8,3 +8,51 @@ router.get('/', (req, res, next) => {
 })
 
 module.exports = router
+
+const prueba = async () => {
+  const Influx = require('influx')
+  const influx = new Influx.InfluxDB({
+    host: 'localhost',
+    database: 'suds',
+    schema: [
+      {
+        measurement: 'ph',
+        fields: {
+          value: Influx.FieldType.FLOAT
+        },
+        tags: [
+          'sensor'
+        ]
+      }
+    ]
+  })
+
+  influx.writePoints([
+    {
+      measurement: 'ph',
+      tags: { sensor: 'entrada' },
+      fields: { value: 30 },
+      timestamp: new Date()
+    },
+    {
+      measurement: 'ph',
+      tags: { sensor: 'entrada' },
+      fields: { value: 30 },
+      timestamp: new Date()
+    }
+  ])
+
+  let rows = await influx.query(`
+      select * from ph
+      where sensor = ${Influx.escape.stringLit('entrada')}
+      order by time desc
+      limit 1000
+  `)
+
+  console.log(rows)
+}
+
+// prueba()
+// .then(() => {
+
+// })
