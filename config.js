3e60e769dbb7a85d80219eed991f7d50a450aaea
Sebastian Barragan <js.barragan796@uniandes.edu.co>
2018-09-25 13:28:04 -0500

    server changes

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
-const isDev = process.env.DEV
-
+const isDev = process.env.DEV === "true"
 module.exports = {
   BACKEND_PORT: process.env.BACKEND_PORT,
   INFLUX_DB_URL: isDev ? process.env.INFLUX_DB_URL_DEV : process.env.INFLUX_DB_URL,
@@ -10,7 +9,6 @@ module.exports = {
   INFLUX_DB_PASSWORD: isDev ? process.env.INFLUX_DB_PASSWORD_DEV : process.env.INFLUX_DB_PASSWORD,
   INFLUX_DB_PROTOCOL: isDev ? process.env.INFLUX_DB_PROTOCOL_DEV : process.env.INFLUX_DB_PROTOCOL,
   PUBLIC_PATH: process.env.PUBLIC_PATH,
-
   QUERY_MUST_HAVE_MEASUREMENT_TYPE: 'Debe especificar el tipo de medición',
   QUERY_MUST_HAVE_FUNCTION_AND_TIME_RANGE: 'Debe especificar función e intervalos de agrupamiento'
 }
--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(cors())
 app.use(express.static(path.join(__dirname, PUBLIC_PATH)))
-app.set('port', BACKEND_PORT)
+
 
 const server = http.createServer(app)
 server.listen(BACKEND_PORT)
