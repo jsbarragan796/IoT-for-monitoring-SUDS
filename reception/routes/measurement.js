ca60bf70b4a7cdcf95a98c34b5c99ab304ed63e9
Sebastian Barragan <js.barragan796@uniandes.edu.co>
2018-11-24 11:41:15 -0500

    Reception modified to process data protocol

--- reception/routes/measurement.js
+++ reception/routes/measurement.js
@@ -3,21 +3,59 @@ module.exports = (producer) => {
   const express = require('express')
   const router = express.Router()
 
-  const { sendMeasurementMessage } = require('../tools')
+  const { sendMeasurementMessage, sendHealdCheck, eventBegun } = require('../tools')
 
   const { SENSOR_SECRET_TOKEN } = require('../config')
+
+  const getMeasurementType = (sensorId, cannel) => {
+    if (sensorId === '4D1089') {
+      if (cannel === '2') return 'rain'
+      else { return 'NaN' }
+    // 4D10B3
+    } else {
+      if (cannel === '0') {
+        return 'conductivity'
+      } else if (cannel === '1') {
+        return 'level'
+      } else {
+        return 'NaN'
+      }
+    }
+  }
+  const getMessageValue = (message) => {
+    return String(message).substr(2, 6)
+  }
+  const getCannel = (message) => {
+    return String(message).substr(1, 1)
+  }
 
   router.post('/', async (req, res, next) => {
     const { body, headers: { authorization } } = req
     if (authorization !== SENSOR_SECRET_TOKEN) res.sendStatus(401)
     else {
-      const { sensorId, measurementType, value, timestamp } = body
-
+      const { sensorId, value, timestamp } = body
       const ts = timestamp * 1000000000
-      const val = Number(value)
-
       try {
-        await sendMeasurementMessage(producer, sensorId, measurementType, val, ts)
+        if (Number(value) === 0) await sendHealdCheck(producer, sensorId, ts)
+        else if (String(value).substr(0, 0) === '9' && String(value).substr(2, 2) === '1') {
+          await eventBegun(producer, sensorId, ts)
+        } else {
+          const messagePart1 = String(value).substr(0, 6)
+          const messagePart2 = String(value).substr(6, 11)
+
+          const valM1 = getMessageValue(messagePart1)
+          const cannelM1 = getCannel(messagePart1)
+          const measurementTypeM1 = getMeasurementType(sensorId, cannelM1)
+
+          await sendMeasurementMessage(producer, sensorId, measurementTypeM1, valM1, ts)
+
+          if (messagePart1 !== messagePart2) {
+            const valM2 = getMessageValue(messagePart2)
+            const cannelM2 = getCannel(messagePart2)
+            const measurementTypeM2 = getMeasurementType(sensorId, cannelM2)
+            await sendMeasurementMessage(producer, sensorId, measurementTypeM2, valM2, ts)
+          }
+        }
         res.sendStatus(200)
       } catch (e) {
         console.log(e)
--- reception/tools/index.js
+++ reception/tools/index.js
@@ -1,9 +1,17 @@
 
-const { KAFKA_TOPIC } = require('../config')
+const { KAFKA_TOPIC, KAFKA_TOPIC_HEALTHCHECK, KAFKA_TOPIC_EVENT_BEGUN } = require('../config')
 
 module.exports = {
   sendMeasurementMessage: (producer, sensorId, measurementType, val, ts) => {
     const message = `${sensorId}_$_${measurementType}_$_${val}_$_${ts}`
     producer.produce(KAFKA_TOPIC, null, Buffer.from(message))
+  },
+  sendHealdCheck: (producer, sensorId, ts) => {
+    const message = `${sensorId}_$_${ts}`
+    producer.produce(KAFKA_TOPIC_HEALTHCHECK, null, Buffer.from(message))
+  },
+  eventBegun: (producer, sensorId, ts) => {
+    const message = `${sensorId}_$_${ts}`
+    producer.produce(KAFKA_TOPIC_EVENT_BEGUN, null, Buffer.from(message))
   }
 }
