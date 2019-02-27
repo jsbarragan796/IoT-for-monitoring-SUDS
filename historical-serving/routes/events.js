c560cdafe754ec56f2ec92faa6c40a570357d200
sebastian <sebas>
2019-02-27 07:37:35 -0500

    some refactoring

--- historical-serving/routes/events.js
+++ historical-serving/routes/events.js
@@ -4,115 +4,9 @@ const csv = require('fast-csv')
 
 const EventLogic = require('../logic/event')
 
-const measurementLogic = require('./../logic/measurement')
+const convertor = require('./../logic/convertor')
 
-const EVENT_PAGINATION = 3
-
-// const ENTRY_SENSOR_ID = '4D1089'
-// const EXIT_SENSOR_ID = '4D1080'
-// const ENTRY_SENSOR_ID = '4D10B3'
-// const EXIT_SENSOR_ID = '4D10B4'
-const MEASUREMENT = [
-  { name: 'level', entry: '4D10B3', exit: '4D10B4', timeIntervalMinutes: '1m', aggregationFunction: 'mean' },
-  { name: 'rain', entry: '4D10B3', timeIntervalMinutes: '60m', timeIntervalMinutesRealTime: '10m', aggregationFunction: 'sum' },
-  { name: 'conductivity', entry: '4D10B3', exit: '4D10B4', timeIntervalMinutes: '1m', aggregationFunction: 'mean' }
-]
-
-let realtimeEventPaginator = {}
-let dateGotRealtimeEvent = new Date().getTime()
-
-const eventSearchRange = async (pageNumber, numberOfEvents) => {
-  const numberOfPages = Math.ceil(numberOfEvents / EVENT_PAGINATION)
-  if (!pageNumber) {
-    throw new Error('No number page given')
-  } else if (pageNumber > numberOfPages || pageNumber <= 0) {
-    throw new Error('Page out of range')
-  } else {
-    const indexFirstEventPage = (pageNumber - 1) * EVENT_PAGINATION
-    const eventsInPage = indexFirstEventPage + EVENT_PAGINATION > numberOfEvents ? numberOfEvents - indexFirstEventPage : EVENT_PAGINATION
-    return { numberOfPages, indexFirstEventPage, eventsInPage }
-  }
-}
-
-const loadHistoricalMesuarementsEvents = async (event) => {
-  for (let index = 0; index < MEASUREMENT.length; index++) {
-    const measurement = MEASUREMENT[index]
-    if (measurement.entry && measurement.name !== 'rain') {
-      event[`entry${measurement.name}`] = await measurementLogic.getMeasurements(measurement.name, measurement.entry, event.startDate, event.finishDate, measurement.aggregationFunction, measurement.timeIntervalMinutes)
-    }
-    if (measurement.exit) {
-      event[`exit${measurement.name}`] = await measurementLogic.getMeasurements(measurement.name, measurement.exit, event.startDate, event.finishDate, measurement.aggregationFunction, measurement.timeIntervalMinutes)
-    }
-    if (measurement.name === 'rain') {
-      event[`entry${measurement.name}`] = await measurementLogic.getMeasurements('level', measurement.entry, event.startDate, event.finishDate, measurement.aggregationFunction, measurement.timeIntervalMinutes)
-    }
-  }
-  return event
-}
-
-const loadRealtimeMesuarementsEvents = async (events) => {
-  const eventsWithMeasurements = []
-  for (let index = 0; index < events.length; index++) {
-    const event = { startDate: parseInt(events[index].startDate),
-      _id: events[index]._id,
-      lastMeasurementDate: parseInt(events[index].lastMeasurementDate)
-    }
-    for (let i = 0; i < MEASUREMENT.length; i++) {
-      const measurement = MEASUREMENT[i]
-      if (measurement.entry && measurement.name !== 'rain') {
-        event[`entry${measurement.name}`] = await measurementLogic.getMeasurements(measurement.name, measurement.entry, event.startDate, event.lastMeasurementDate, measurement.aggregationFunction, measurement.timeIntervalMinutes)
-      }
-      if (measurement.exit) {
-        event[`exit${measurement.name}`] = await measurementLogic.getMeasurements(measurement.name, measurement.exit, event.startDate, event.lastMeasurementDate, measurement.aggregationFunction, measurement.timeIntervalMinutes)
-      }
-      if (measurement.name === 'rain') {
-        event[`entry${measurement.name}`] = await measurementLogic.getMeasurements('level', measurement.entry, event.startDate, event.lastMeasurementDate, measurement.aggregationFunction, measurement.timeIntervalMinutesRealTime)
-      }
-    }
-    eventsWithMeasurements.push(event)
-  }
-  return eventsWithMeasurements
-}
-
-router.get('/current-events', async (req, res) => {
-  const currentDate = new Date().getTime()
-  try {
-    if (currentDate - dateGotRealtimeEvent > 2500) {
-      const pageNumber = parseInt(req.query.pageNumber)
-      const numberOfEvents = await EventLogic.numberOfNotEndedEvents()
-      if (numberOfEvents > 0) {
-        const searchRange = await eventSearchRange(pageNumber, numberOfEvents)
-        const events = await EventLogic.findNotFinishedEvents(searchRange.indexFirstEventPage, searchRange.eventsInPage)
-        const eventsWithMeasurements = await loadRealtimeMesuarementsEvents(events)
-
-        const paginator = { 'currentPage': pageNumber, 'totalPages': searchRange.numberOfPages, 'events': eventsWithMeasurements }
-        realtimeEventPaginator = paginator
-        dateGotRealtimeEvent = currentDate
-        res.send(paginator)
-      } else {
-        const paginator = { 'currentPage': 0, 'totalPages': 0, 'events': [] }
-        res.send(paginator)
-      }
-    } else {
-      res.send(realtimeEventPaginator)
-    }
-  } catch (e) {
-    res.status(400).send(e.message)
-  }
-})
-
-router.get('/are-current-events', async (req, res) => {
-  try {
-    const numberOfEvents = await EventLogic.numberOfNotEndedEvents()
-    if (numberOfEvents > 0) {
-      res.send({ RTEvnets: true })
-    } else {
-      res.send({ RTEvnets: false })
-    }
-  } catch (e) {
-    res.status(400).send(e.message)
-  }
-})
+convertor.setupSensors()
 
 router.post('/filtered-data', async (req, res) => {
   try {
@@ -120,7 +14,7 @@ router.post('/filtered-data', async (req, res) => {
     const filterData = req.body
     const numberOfEvents = await EventLogic.numberOfFilteredEvents(filterData)
 
-    const searchRange = await eventSearchRange(pageNumber, numberOfEvents)
+    const searchRange = await convertor.eventSearchRange(pageNumber, numberOfEvents)
 
     const events = await EventLogic.findFinishedFilteredEvents(searchRange.indexFirstEventPage, searchRange.eventsInPage, filterData)
     const paginator = { 'numberOfEvents': numberOfEvents, 'currentPage': pageNumber, 'totalPages': searchRange.numberOfPages, 'events': events }
@@ -138,7 +32,7 @@ router.get('/data', async (req, res) => {
   try {
     const eventId = req.query.eventId
     const event = await EventLogic.findEvent(eventId)
-    const eventsWithMeasurements = await loadHistoricalMesuarementsEvents(event)
+    const eventsWithMeasurements = await convertor.loadHistoricalMesuarementsEvents(event)
     res.send(eventsWithMeasurements)
   } catch (e) {
     res.status(200).send({
@@ -150,7 +44,7 @@ router.get('/get-csv', async (req, res) => {
   try {
     const eventId = req.query.eventId
     const event = await EventLogic.findEvent(eventId)
-    const eventsWithMeasurements = await loadHistoricalMesuarementsEvents(event)
+    const eventsWithMeasurements = await convertor.loadHistoricalMesuarementsEvents(event)
     const date = new Date(event.startDate / 1e6).toLocaleDateString().replace(/\//g, '_')
     const fileName = `evento_${date}.csv`
     res.writeHead(200, {
@@ -187,5 +81,4 @@ router.get('/get-csv', async (req, res) => {
     })
   }
 })
-
 module.exports = router
