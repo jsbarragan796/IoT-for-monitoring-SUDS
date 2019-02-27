aa3ec860fbb434e050b9a7a0a08cacf1db2f997a
sebastian <sebas>
2019-02-27 07:37:44 -0500

    socket logic

---
+++ historical-serving/logic/convertor.js
@@ -0,0 +1,107 @@
+const { MONGODB_URI } = require('../config')
+const MongoClient = require('mongodb').MongoClient
+const measurementLogic = require('./measurement')
+const EVENT_PAGINATION = 3
+
+// const ENTRY_SENSOR_ID = '4D1089'
+// const EXIT_SENSOR_ID = '4D1080'
+// const ENTRY_SENSOR_ID = '4D10B3'
+// const EXIT_SENSOR_ID = '4D10B4'
+// const MEASUREMENT = [
+//   { name: 'level', entry: '4D10B3', exit: '4D10B4', timeIntervalMinutes: '1m', aggregationFunction: 'mean' },
+//   { name: 'rain', entry: '4D10B3', timeIntervalMinutes: '60m', timeIntervalMinutesRealTime: '10m', aggregationFunction: 'sum' },
+//   { name: 'conductivity', entry: '4D10B3', exit: '4D10B4', timeIntervalMinutes: '1m', aggregationFunction: 'mean' }
+// ]
+
+const MEASUREMENT = []
+
+const mongoConnect = () => {
+  return new Promise((resolve, reject) => {
+    MongoClient.connect(MONGODB_URI, { useNewUrlParser: true }, async (err, client) => {
+      if (err) reject(err)
+      else {
+        resolve(client)
+      }
+    })
+  })
+}
+
+module.exports = {
+  setupSensors: async () => {
+    if (MEASUREMENT.length === 0) {
+      const mongo = await mongoConnect()
+      const Sensor = mongo.db().collection('Sensor')
+      const sensors = await Sensor.find({}).toArray()
+      const MeasurementType = mongo.db().collection('MeasurementType')
+      const measurementTypes = await MeasurementType.find({}).toArray()
+      mongo.close()
+
+      for (let measurement of measurementTypes) {
+        const locationSensors = sensors.filter(({ type }) => {
+          return measurement._id.toString() === type.toString()
+        })
+        const tempMeasurement = measurement
+        locationSensors.forEach(sensor => {
+          if (sensor.isEntrance) {
+            tempMeasurement.entry = sensor.id.split('-')[0]
+          }
+          if (!sensor.isEntrance) {
+            tempMeasurement.entry = sensor.id.split('-')[0]
+          }
+        })
+        MEASUREMENT.push(tempMeasurement)
+        console.log('tempMeasurement added ', tempMeasurement)
+      }
+    }
+  },
+  eventSearchRange: async (pageNumber, numberOfEvents) => {
+    const numberOfPages = Math.ceil(numberOfEvents / EVENT_PAGINATION)
+    if (!pageNumber) {
+      throw new Error('No number page given')
+    } else if (pageNumber > numberOfPages || pageNumber <= 0) {
+      throw new Error('Page out of range')
+    } else {
+      const indexFirstEventPage = (pageNumber - 1) * EVENT_PAGINATION
+      const eventsInPage = indexFirstEventPage + EVENT_PAGINATION > numberOfEvents ? numberOfEvents - indexFirstEventPage : EVENT_PAGINATION
+      return { numberOfPages, indexFirstEventPage, eventsInPage }
+    }
+  },
+  loadHistoricalMesuarementsEvents: async (event) => {
+    for (let index = 0; index < MEASUREMENT.length; index++) {
+      const measurement = MEASUREMENT[index]
+      if (measurement.entry && measurement.name !== 'rain') {
+        event[`entry${measurement.name}`] = await measurementLogic.getMeasurements(measurement.name, measurement.entry, event.startDate, event.finishDate, measurement.aggregationFunction, measurement.timeIntervalMinutes)
+      }
+      if (measurement.exit) {
+        event[`exit${measurement.name}`] = await measurementLogic.getMeasurements(measurement.name, measurement.exit, event.startDate, event.finishDate, measurement.aggregationFunction, measurement.timeIntervalMinutes)
+      }
+      if (measurement.name === 'rain') {
+        event[`entry${measurement.name}`] = await measurementLogic.getMeasurements('level', measurement.entry, event.startDate, event.finishDate, measurement.aggregationFunction, measurement.timeIntervalMinutes)
+      }
+    }
+    return event
+  },
+  loadRealtimeMesuarementsEvents: async (events) => {
+    const eventsWithMeasurements = []
+    for (let index = 0; index < events.length; index++) {
+      const event = { startDate: parseInt(events[index].startDate),
+        _id: events[index]._id,
+        lastMeasurementDate: parseInt(events[index].lastMeasurementDate)
+      }
+      for (let i = 0; i < MEASUREMENT.length; i++) {
+        const measurement = MEASUREMENT[i]
+        if (measurement.entry && measurement.name !== 'rain') {
+          event[`entry${measurement.name}`] = await measurementLogic.getMeasurements(measurement.name, measurement.entry, event.startDate, event.lastMeasurementDate, measurement.aggregationFunction, measurement.timeIntervalMinutes)
+        }
+        if (measurement.exit) {
+          event[`exit${measurement.name}`] = await measurementLogic.getMeasurements(measurement.name, measurement.exit, event.startDate, event.lastMeasurementDate, measurement.aggregationFunction, measurement.timeIntervalMinutes)
+        }
+        if (measurement.name === 'rain') {
+          event[`entry${measurement.name}`] = await measurementLogic.getMeasurements('level', measurement.entry, event.startDate, event.lastMeasurementDate, measurement.aggregationFunction, measurement.timeIntervalMinutesRealTime)
+        }
+      }
+      eventsWithMeasurements.push(event)
+    }
+    return eventsWithMeasurements
+  }
+}
--- historical-serving/realTimeService/realTime.js
+++ historical-serving/realTimeService/realTime.js
@@ -1,32 +1,54 @@
 
 const io = require('socket.io')()
 const EventLogic = require('../logic/event')
-const measurementLogic = require('./../logic/measurement')
+const convertor = require('./../logic/convertor')
 
-let realtimeEventPaginator = {}
+let eventsWithMeasurements = []
 let dateGotRealtimeEvent = new Date().getTime()
+let numberOfEvents = 0
+convertor.setupSensors()
 
-const currentsEvents = async (pageNumberParameter) => {
+setInterval(async () => {
+  numberOfEvents = await EventLogic.numberOfNotEndedEvents()
+  const currentDate = new Date().getTime()
+  if (numberOfEvents > 0) {
+    const notEndedEvents = await EventLogic.findNotFinishedEvents(0, numberOfEvents)
+    eventsWithMeasurements = await convertor.loadRealtimeMesuarementsEvents(notEndedEvents)
+    dateGotRealtimeEvent = currentDate
+  }
+  console.log('actualizando 223123')
+}, 900)
+
+const currentsEvents = async (pageNumberParameter, lastIndex, client) => {
   const currentDate = new Date().getTime()
   if (currentDate - dateGotRealtimeEvent > 2500) {
     const pageNumber = Number(pageNumberParameter)
     const numberOfEvents = await EventLogic.numberOfNotEndedEvents()
     if (numberOfEvents > 0) {
-      const searchRange = await eventSearchRange(pageNumber, numberOfEvents)
+      const searchRange = await convertor.eventSearchRange(pageNumber, numberOfEvents)
       const events = await EventLogic.findNotFinishedEvents(searchRange.indexFirstEventPage, searchRange.eventsInPage)
-      const eventsWithMeasurements = await loadRealtimeMesuarementsEvents(events)
-
+      const eventsWithMeasurements = await convertor.loadRealtimeMesuarementsEvents(events)
       const paginator = { 'currentPage': pageNumber, 'totalPages': searchRange.numberOfPages, 'events': eventsWithMeasurements }
       realtimeEventPaginator = paginator
       dateGotRealtimeEvent = currentDate
-      res.send(paginator)
+      return paginator
     } else {
       const paginator = { 'currentPage': 0, 'totalPages': 0, 'events': [] }
-      res.send(paginator)
+      return paginator
     }
   } else {
-    res.send(realtimeEventPaginator)
+    return realtimeEventPaginator
   }
+}
+
+const areCurrentEnvents = (client) => {
+  setInterval(async () => {
+    if (numberOfEvents > 0) {
+      client.emit('are-current-events', true)
+    } else {
+      client.emit('are-current-events', false)
+    }
+  }, 1000)
 }
 
 module.exports = {
@@ -37,6 +59,12 @@ module.exports = {
         setInterval(() => {
           client.emit('timer', `${new Date()}-servidor`)
         }, interval)
+      })
+      client.on('are-current-events', () => {
+        areCurrentEnvents(client)
+      })
+      client.on('current-events', (pageNumberParameter, lastIndex) => {
+        currentsEvents(pageNumberParameter, lastIndex, client)
       })
     })
     io.listen(PORT)
